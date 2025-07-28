60d7dcbbb72d41d5f1d55fdebdef35601350f5f6
Lekan Adeyeri <lekanadeyeri@lekans-air.lan>
2025-07-28 00:49:31 -0400

    update colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
+        'text-black bg-[#c6dcc9] dark:text-white dark:bg-black',
         GeistSans.variable,
         GeistMono.variable
       )}
