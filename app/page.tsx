39ab2d75f5673b97fe8ba8bdc0b5e7c56b9ed392
Lekan Adeyeri <lekanadeyeri@lekans-air.lan>
2025-07-28 16:24:03 -0400

    added more posts, updated blurb

--- app/page.tsx
+++ app/page.tsx
@@ -15,16 +15,16 @@ export default function Page() {
        <a href="https://www.asnr.org/cornelius-g-dyke-award/" className="underline">Cornelius G. Dyke Award from ASNR</a>
        {`. I've interned as SWE+PM at `}
        <a href="http://microsoft.com/" className="underline">Microsoft</a>
-       {` working with GPT-3 and microservices, and been recognized as a Kleiner Perkins Engineering Fellowship Finalist and `}
-       <a href="https://twitter.com/BessemerVP/status/1547271355539734528" className="underline">Bessemer Venture Partners Fellow participant</a>
-       {` before taking a break for reasons including exploring emerging technologies unconstrained. Previously, I've built `}
+       {` working with GPT-3 and microservices, and I've explored various venture capital `}
+       <a href="https://twitter.com/BessemerVP/status/1547271355539734528" className="underline">fellowships</a>
+       {` before taking a break to explore emerging technologies unconstrained. Previously, I've built `}
        <a href="https://github.com/lekanadeyeri/Commonwealth-Honors-Project" className="underline">computer vision tools for autism support</a>
        {`, grocery carpooling platforms with 450+ users in one target location with a $30 budget, and `}
        <a href="https://github.com/lekandigital/InjuryEmotion" className="underline">hackathon-winning worker sentiment analysis systems</a>
        {` using computer vision in industrial contexts. I focus on building systems that amplify human creativity rather than replace it.`}
      </p>
      <div className="my-8">
-       <h2 className="mb-4 text-xl font-semibled tracking-tight">
+       <h2 className="mb-4 text-xl font-semibold tracking-tight">
          Selected Essays
        </h2>
        <EssaysPosts />
